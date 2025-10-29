4df59b4e8323f049f85688b3784b12554650e148
Sameer Kumar <sameerkumar20252@gmail.com>
2025-10-29 21:34:50 +0530

    H

---
+++ Javascript/Day5/index.js
@@ -0,0 +1,80 @@
+//number
+// let a = 10;
+// let b = 245.589;
+// let c = b.toFixed(1);
+// console.log(c);   //ans = 245.6
+// console.log(typeof c)   //string
+
+// console.log(b.toPrecision(4))    //ans = 245.5
+// console.log(b.toString());
+
+// let a = new Number(20);    // type =  object
+// let b = new Number(20);
+
+// console.log(a==b)  // ans= false
+
+// console.log(Boolean(null))
+
+
+// let b = 20;
+// console.log(typeof b);
+
+// let obj1 = {
+//     name: "Sameer"
+// }
+// let obj2 = {
+//     name: "Ramesh"
+// }
+
+// console.log(obj1==obj2)   //false becz both objects point same different memory
+
+// let obj1 = {
+//     name: "Sameer"
+// }
+// let obj2 = obj1;
+
+// console.log(obj1==obj2)   //true becz both objects point same memory
+
+// Non Primitive: Reference bases pe compare
+// Primitive: Copy by value
+// let a = 10;
+// let b = a;
+
+// console.log(a==b);   //true
+
+//Math funtion
+
+// console.log(Math.abs(-4.36));  // Same as mode |x| fuhntion
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.sqrt(4));
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(6.3));
+// console.log(Math.log10(20));
+// console.log(Math.max(20,11,3421,12));
+
+// console.log(Math.random());     
+// [0,1): It will generate value between 0 and 1, where 0 is included but 1 is not included
+
+// Satebaaji ek game banata:(0-9)
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+// console.log(Math.floor(Math.random()*6)+1);
+
+// Math.floor(Math.random()*totalNumberOfOutcome)+shift
+
+
+// 15-25
+
+
+// console.log(Math.floor(Math.random()*11)+15)
+
+// Math.floor(Math.random()*(max-min+1))+min
+
+// OTP Generate: 4 digit: 1000-9999
+
+
+// console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
+
+// console.log(Math.floor(Math.random()*100)+1)  //[1,100)
---
+++ Javascript/first.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+// console.log("2+3")
+// let a=10;
+// console.log(a);
+
+// const id1=Symbol("Id");
+// console.log(id1);
+
+let arr=[9,78.9,"Sameer",true];
+console.log(arr);
+console.log(typeof arr);
+console.log(Array.isArray(arr));
